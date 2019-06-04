a9a08f1ac3f5ec986d7c54b9b2ba8d361d884b9e
blusky10 <blusky0910@gmail.com>
2019-06-04 22:59:54 +0900

    Update: subset 추가

--- src/main/java/references/Subset.java
+++ src/main/java/references/Subset.java
@@ -1,4 +1,29 @@
 package references;
 
+import java.util.Stack;
+import java.util.stream.Collectors;
+
+// 부분집합
 public class Subset {
+
+    int N = 3;
+    Stack<Integer> subset = new Stack<>();
+
+    public static void main(String[] args) {
+        Subset subset = new Subset();
+        subset.search(1);
+    }
+
+    public void search(int k){
+
+        if (k == N+1 ){
+            System.out.println(subset.stream().collect(Collectors.toList()));
+        }else{
+            subset.push(k);
+            search(k+1);
+            subset.pop();
+            search(k+1);
+        }
+    }
+
 }
