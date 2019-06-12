8a124bdc0d0d88bcd497f03e6423f7c4eb132dad
blusky0910@gmail.com <blusky0910@gmail.com>
2019-06-12 09:04:42 +0900

    Update: Coin 구하기

---
+++ src/main/java/references/Coin.java
@@ -0,0 +1,26 @@
+package references;
+
+// 최소 개수 동전 구하기
+public class Coin {
+
+    static int N=10;
+    static int[] ready = new int[N+1];
+    static int[] value = new int[N+1];
+    static int[] data = {1, 3, 4};
+
+    public static void main(String[] args) {
+
+        value[0] = 0;
+        for (int x=1; x<=N; x++){
+            value[x] = 100;
+
+            for (int c : data){
+                if (x-c >=0){
+                    value[x] = Math.min(value[x], value[x-c]+1);
+                }
+            }
+        }
+
+        System.out.println(value[8]);
+    }
+}
