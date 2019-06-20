5e5123a206d3b43c3cd4730adeab50d50111327a
blusky0910@gmail.com <blusky0910@gmail.com>
2019-06-21 08:57:18 +0900

    Update: 가위바위보

---
+++ src/main/java/microbit/ex4.js
@@ -0,0 +1,31 @@
+let num = 0
+input.onGesture(Gesture.Shake, function () {
+    num = Math.randomRange(0, 2)
+})
+basic.forever(function () {
+    if (num == 0) {
+        basic.showLeds(`
+            # # . . #
+            # # . # .
+            . . # . .
+            # # . # .
+            # # . . #
+            `)
+    } else if (num == 1) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else if (num == 2) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+    }
+})
