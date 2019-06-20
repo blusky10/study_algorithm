a9c1f6658bccd0422e2e9f15dca18ada8289362a
blusky0910@gmail.com <blusky0910@gmail.com>
2019-06-21 08:56:12 +0900

    Update: 카운터기

---
+++ src/main/java/microbit/ex3.js
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    num += 1
+    basic.showNumber(num)
+})
+input.onButtonPressed(Button.B, function () {
+    num += -1
+    basic.showNumber(num)
+})
+input.onButtonPressed(Button.AB, function () {
+    num = 0
+    basic.showNumber(num)
+})
+let num = 0
+num = 0
+basic.showNumber(num)
