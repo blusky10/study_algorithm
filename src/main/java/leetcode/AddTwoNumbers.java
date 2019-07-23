4f6e9dc0d270b75ec23671a36ef892c3936a78f8
blusky0910@gmail.com <blusky0910@gmail.com>
2019-07-23 17:27:20 +0900

    Update: leetcode AddTowNumber complete

--- src/main/java/leetcode/AddTwoNumbers.java
+++ src/main/java/leetcode/AddTwoNumbers.java
@@ -1,43 +1,63 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //Add Two Numbers
 //Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 //Output: 7 -> 0 -> 8
 //Explanation: 342 + 465 = 807.
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = null;
+        int add = 0;
+        List<Integer> resultList = new ArrayList<>();
+        while(true){
+            int sum = l1.val + l2.val + add;
+            int val = 0;
 
-        long i = makeNumber(l1) + makeNumber(l2);
-        return makeNumberToListNode(i);
-    }
+            if (sum >=10){
+                val = sum % 10;
+                add = 1;
+            }else{
+                val = sum;
+                add = 0;
+            }
 
-    public long makeNumber(ListNode l){
-        ListNode node = l;
-        long result = 0;
-        int i=0;
+            resultList.add(val);
 
-        while(node != null){
-            result += node.val * (Math.pow(10,i));
-            i++;
-            node = node.next;
+            if (l1.next == null && l2.next == null){
+                if (add != 0){
+                    resultList.add(add);
+                }
+                break;
+            }else if (l1.next != null && l2.next == null){
+                l1 = l1.next;
+                l2.val = 0;
+            }else if (l1.next == null && l2.next != null){
+                l1.val = 0;
+                l2 = l2.next;
+            }else{
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
         }
 
+        result = makeListToListNode(resultList);
         return result;
     }
 
-    public ListNode makeNumberToListNode(long n){
-        String number = Double.toString(n);
+    public ListNode makeListToListNode(List<Integer> list){
+        int index = list.size()-1;
+        ListNode listNode = new ListNode(list.get(index));
 
-        char[] chars = number.toCharArray();
-
-        ListNode result = new ListNode(Character.getNumericValue(chars[0]));
-
-        for (int i=1; i<chars.length; i++){
-            ListNode now = new ListNode(Character.getNumericValue(chars[i]));
-            now.next = result;
-            result = now;
+        for (int i=index-1; i>=0; i--){
+            ListNode temp = new ListNode(list.get(i));
+            temp.next = listNode;
+            listNode = temp;
         }
-        return result;
+        return listNode;
     }
 }
 
--- src/test/java/leetcode/AddTowNumbersTest.java
+++ src/test/java/leetcode/AddTowNumbersTest.java
@@ -3,9 +3,6 @@ package leetcode;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class AddTowNumbersTest {
 
     AddTwoNumbers addTwoNumbers = new AddTwoNumbers();
@@ -20,112 +17,16 @@ public class AddTowNumbersTest {
         ll2.next = ll3;
 
 
-        ListNode l1 = new ListNode(9);
-        ListNode l2 = new ListNode(9);
-        ListNode l3 = new ListNode(9);
-        ListNode l4 = new ListNode(9);
-        ListNode l5 = new ListNode(9);
-        ListNode l6 = new ListNode(9);
-        ListNode l7 = new ListNode(9);
-        ListNode l8 = new ListNode(9);
-        ListNode l9 = new ListNode(9);
-        ListNode l10 = new ListNode(1);
+        ListNode l1 = new ListNode(5);
+        ListNode l2 = new ListNode(6);
+        ListNode l3 = new ListNode(4);
         l1.next = l2;
         l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-        l5.next = l6;
-        l6.next = l7;
-        l7.next = l8;
-        l8.next = l9;
-        l9.next = l10;
 
         ListNode listNode = addTwoNumbers.addTwoNumbers(ll1, l1);
 
         Assert.assertEquals(7, listNode.val);
 
     }
-    @Test
-    public void makeNumberToListNodeTest(){
-        ListNode listNode = addTwoNumbers.makeNumberToListNode(807);
-
-        while (listNode.next != null){
-            System.out.println(listNode.val);
-            listNode = listNode.next;
-        }
-    }
-
-
-    @Test
-    public void makeNumberTest(){
-        ListNode l1 = new ListNode(9);
-        ListNode l2 = new ListNode(9);
-        ListNode l3 = new ListNode(9);
-        ListNode l4 = new ListNode(9);
-        ListNode l5 = new ListNode(0);
-        ListNode l6 = new ListNode(9);
-        ListNode l7 = new ListNode(9);
-        ListNode l8 = new ListNode(9);
-        ListNode l9 = new ListNode(9);
-        ListNode l10 = new ListNode(1);
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-        l5.next = l6;
-        l6.next = l7;
-        l7.next = l8;
-        l8.next = l9;
-        l9.next = l10;
-
-        long l = addTwoNumbers.makeNumber(l1);
-
-        System.out.println(l);
-    }
-
-    @Test
-    public void test1(){
-        long i = Long.parseLong("1000000000000000000000000000001");
-
-        System.out.println(i);
-    }
-
-
-    @Test
-    public void makeNumberTest1(){
-        ListNode l1 = new ListNode(1);
-        ListNode l2 = new ListNode(0);
-        ListNode l3 = new ListNode(0);
-        ListNode l4 = new ListNode(0);
-        ListNode l5 = new ListNode(0);
-        ListNode l6 = new ListNode(0);
-        ListNode l7 = new ListNode(0);
-        ListNode l8 = new ListNode(0);
-        ListNode l9 = new ListNode(0);
-        ListNode l10 = new ListNode(0);
-        ListNode l11 = new ListNode(1);
-        l1.next = l2;
-        l2.next = l3;
-        l3.next = l4;
-        l4.next = l5;
-        l5.next = l6;
-        l6.next = l7;
-        l7.next = l8;
-        l8.next = l9;
-        l9.next = l10;
-        l10.next = l11;
-
-//[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
-        System.out.println(addTwoNumbers.makeNumber(l1));
-
-        System.out.println(Long.MAX_VALUE);
-        System.out.println(Float.MAX_VALUE);
-        System.out.println(Double.MAX_VALUE);
-        System.out.println(Math.pow(10, 31) * 1 + 664);
-
-        Double d = Math.pow(10, 31) * 1 + 664;
-        System.out.println(d.longValue());
-    }
-
 
 }
