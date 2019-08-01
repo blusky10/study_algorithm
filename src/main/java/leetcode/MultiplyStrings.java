1793ee84f110c52722cd7c464abe6249d572533e
blusky0910@gmail.com <blusky0910@gmail.com>
2019-08-01 17:27:09 +0900

    Update: multiplyString

--- src/main/java/leetcode/MultiplyStrings.java
+++ src/main/java/leetcode/MultiplyStrings.java
@@ -1,6 +1,8 @@
 package leetcode;
 
 
+import java.util.Stack;
+
 //Input: num1 = "2", num2 = "3"
 //Output: "6"
 //Example 2:
@@ -14,26 +16,70 @@ package leetcode;
 //Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 //You must not use any built-in BigInteger library or convert the inputs to integer directly.
 public class MultiplyStrings {
-
     public static void main(String[] args) {
-        String num1 = "498828660196";
-        String num2 = "840477629533";
-        System.out.println(getNum(num1));
-        System.out.println(getNum(num2));
-        System.out.println(multiply(num1, num2));
-    }
-    public static String multiply(String num1, String num2) {
-        long i = (getNum(num1) * getNum(num2));
-        return i + "";
+//        String num1 = "498828660196";
+//        String num2 = "840477629533";
+
+        String num1 = "123";
+        String num2 = "456";
+
+        MultiplyStrings multiplyStrings = new MultiplyStrings();
+        System.out.println(multiplyStrings.multiply(num1, num2));
     }
 
-    public static long getNum(String num){
-        long result = 0;
-        for(int i=0; i<num.length(); i++){
-            char c = num.charAt(i);
-            result += (long)((c-'0')*Math.pow(10, num.length() -1 - i));
+    static int[] data = new int[1000];
+
+    public String multiply(String num1, String num2) {
+        if (num1.length() < num2.length()){
+            String temp = num1;
+            num1 = num2;
+            num2 = temp;
         }
 
-        return result;
+        for(int i=num2.length()-1; i>=0; i--){
+            int n = num2.charAt(i) - '0';
+            int s = num2.length()-i -1;
+            getNum(num1, n, s);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int start = 0;
+        for (int i=data.length-1; i>=0; i--){
+            if (start !=0){
+                sb.append(data[i]);
+            }
+
+            if (data[i] != 0 && start == 0){
+                sb.append(data[i]);
+                start++;
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void getNum(String num, int n, int s){
+        int over = 0;
+        int index = s;
+
+        for(int i=num.length()-1; i>=0; i--){
+            char c = num.charAt(i);
+            int a = (c-'0')*n + over + data[index];
+            int b = a % 10;
+
+            if (a >= 10){
+                over = a / 10;
+            }else{
+                over = 0;
+            }
+            data[index] = b;
+            index++;
+
+        }
+
+        if (over != 0){
+            data[index] +=over;
+            index++;
+        }
     }
 }
