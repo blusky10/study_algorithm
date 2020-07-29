dcaf0c9bbca3754771695dbeec02623850a6626a
KIMSANGHYUN <blusky0910@gmail.com>
2020-07-29 10:41:10 +0900

    Hacker Rank 연습 문제 추가

---
+++ src/main/java/hackerrank/JavaIfElse.java
@@ -0,0 +1,24 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class JavaIfElse {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if (N % 2 == 0){
+            if (N >=6 && N <=20){
+                System.out.println("Weird");
+            }else{
+                System.out.println("Not Weird");
+            }
+        }else{
+            System.out.println("Weird");
+        }
+
+        scanner.close();
+    }
+}
