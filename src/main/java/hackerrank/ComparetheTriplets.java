e4a05a62eed41fb8129ec06b63774f1a8c01d2ba
KIMSANGHYUN <blusky0910@gmail.com>
2020-07-31 19:22:33 +0900

    Compare Triplets 추가

---
+++ src/main/java/hackerrank/ComparetheTriplets.java
@@ -0,0 +1,59 @@
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class ComparetheTriplets {
+
+    // Complete the compareTriplets function below.
+    static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+
+        int sumA = 0;
+        int sumB = 0;
+
+        for (int i =0; i<a.size(); i++){
+            int eA = a.get(i);
+            int eB = b.get(i);
+
+            if (eA > eB){
+                sumA++;
+            }else if (eA < eB){
+                sumB++;
+            }
+        }
+        return Arrays.asList(sumA, sumB);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
