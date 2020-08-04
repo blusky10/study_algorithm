0a818af62ac97ddb24b6c847b10aeea4439a4f10
KIMSANGHYUN <blusky0910@gmail.com>
2020-08-05 08:04:52 +0900

    Warm up 문제 추가

---
+++ src/main/java/hackerrank/BirthdayCakeCandles.java
@@ -0,0 +1,50 @@
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class BirthdayCakeCandles {
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] ar) {
+
+        Arrays.sort(ar);
+        long maxValue = ar[ar.length-1];
+        long count = Arrays.stream(ar).filter(value -> value == maxValue).count();
+
+        int candles = (int)count;
+        return candles;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/hackerrank/MiniMaxSum.java
@@ -0,0 +1,44 @@
+package hackerrank;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+import java.util.stream.IntStream;
+
+public class MiniMaxSum {
+    // Complete the miniMaxSum function below.
+    static void miniMaxSum(int[] arr) {
+
+        Arrays.sort(arr);
+
+        long sum = 0;
+        for (int i=0; i< arr.length;i++){
+            sum+=arr[i];
+        }
+
+        long minSum = sum - arr[4];
+        long maxSum = sum - arr[0];
+        System.out.println(minSum+ " " + maxSum);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
