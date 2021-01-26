2fff913632dcad30a49824f72625da8f3075eb9d
sanghyun-kim <sanghyun0910.kim@samsung.com>
2021-01-26 10:59:14 +0900

    Grading Students

---
+++ src/main/java/hackerrank/GradingStudents.java
@@ -0,0 +1,66 @@
+package hackerrank;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class GradingStudentsResult {
+
+    /*
+     * Complete the 'gradingStudents' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY grades as parameter.
+     */
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        // Write your code here
+        return grades.stream().map(
+                integer -> {
+                    int a = (integer / 5) + 1;
+                    int value = (5 * a) - integer;
+                    if (integer >= 38 && value <= 2){
+                        return a * 5;
+                    }
+                    return integer;
+                }
+        ).collect(toList());
+
+    }
+
+}
+
+public class GradingStudents {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = GradingStudentsResult.gradingStudents(grades);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
